531a3998f28e6a584e1fcba6ac98c9491509a6e9
HamzaUyar <rayuh03@gmail.com>
2024-05-12 22:03:25 +0300

    halis

--- src/app/api/application/[formId]/route.ts
+++ src/app/api/application/[formId]/route.ts
@@ -1,24 +1,33 @@
 import ConnectDB from "@/config/database";
+import { NextRequest, NextResponse } from "next/server";
+import Application from '@/models/Application';
 
-export const GET = async () => {
+export const GET = async (request: NextRequest) => {
     // TODO: Get all applications by given formId from the url
-
     try {
-        ConnectDB();
+        await ConnectDB(); 
+        const url = request.url;
+        const id = url.split('/').pop();
+        const applications = await Application.find({ formId: id });
+        return NextResponse.json({ applications, id }, { status: 200 });
 
     } catch (error) {
-        
+        return NextResponse.json({ message: "Applications Cannot be fetched"}, { status: 401 })
     }
 }
 
-export const DELETE = async () => {
+export const DELETE = async (request: NextRequest) => {
     // TODO: Delete all applications by given formId from the url
     // Bu fonksiyon form silindiğinde de çalıştırılmalı ki veri kirliliğinin önüne geçilebilsin.
 
     try {
-        ConnectDB();
+        await ConnectDB();
+        const url = request.url;
+        const id = url.split('/').pop();
+        const applications = await Application.deleteMany({ formId: id });
+        return NextResponse.json({ applications, id }, { status: 200 });
 
     } catch (error) {
-        
+        return NextResponse.json({ message: "Delete operation failed"}, { status: 401 })
     }
 }
--- src/app/api/form/[formId]/route.ts
+++ src/app/api/form/[formId]/route.ts
@@ -1,36 +1,51 @@
 import ConnectDB from "@/config/database";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import Form from '@/models/Form';
 
-export const GET = async () => {
+export const GET = async (request:NextRequest) => {
     // TODO: Get the form data by given formId from the url
 
     try {
-        ConnectDB();
+        await ConnectDB();
+        const url = request.url;
+        const id = url.split('/').pop();
+        const form = await Form.findById(id);
+        return NextResponse.json({ form }, { status: 200 });
 
     } catch (error) {
         
+        return NextResponse.json({message: "Form Cannot be fetched"}, { status: 401 })
     }
 }
 
 export const UPDATE = async (request : NextRequest) => {
     // TODO: Update the form data by given formId from the url and given request data
-    const { } = request.json();
+    //const { } = request.json();
 
     try {
-        ConnectDB();
+        const data = request.json();
+        await ConnectDB();
+        const url = request.url;
+        const id = url.split('/').pop();
+        const form = await Form.updateOne({ _id: id }, { data });
+        return NextResponse.json({ form, id }, { status: 200 });
 
     } catch (error) {
-        
+        return NextResponse.json({ message: "Form Cannot be updated"}, { status: 401 })
     }
 }
 
-export const DELETE = async () => {
+export const DELETE = async (request : NextRequest) => {
     // TODO: Delete the form data by given formId from the url
 
     try {
-        ConnectDB();
+        await ConnectDB();
+        const url = request.url;
+        const id = url.split('/').pop();
+        const form = await Form.deleteOne({ _id: id });
+        return NextResponse.json({ form, id }, { status: 200 });
 
     } catch (error) {
-        
+         return NextResponse.json({ message: "Delete operation failed"}, { status: 401 })
     }
 }
